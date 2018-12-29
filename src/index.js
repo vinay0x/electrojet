b793c5fabf963e53a1fb7dc0e48f42807a7f7a24
Vinay <vinay@surveysparrow.com>
2018-12-29 21:29:51 +0530

    Added redux-persist

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { PersistGate } from 'redux-persist/integration/react'
 import { Provider } from 'react-redux'
 
-import store from './store'
+import { store, persistor } from './store'
 import App from './App'
 
 ReactDOM.render(
-  <Provider store={store}>
-    <App />
+  <Provider store={ store }>
+    <PersistGate loading={null} persistor={persistor}>
+      <App />
+    </PersistGate>
   </Provider>, document.getElementById('app'))
--- src/store.js
+++ src/store.js
@@ -1,11 +1,24 @@
 import { createStore, applyMiddleware, compose } from 'redux'
+import { persistStore, persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
 import thunk from 'redux-thunk'
-
 import rootReducer from './reducers'
-export default createStore(
-  rootReducer,
+
+const persistConfig = {
+  key: 'root',
+  storage
+}
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+const store = createStore(
+  persistedReducer,
   compose(
     applyMiddleware(thunk),
     window.__REDUX_DEVTOOLS_EXTENSION__ ? window.__REDUX_DEVTOOLS_EXTENSION__() : () => null
   )
 )
+const persistor = persistStore(store)
+
+export {
+  store,
+  persistor
+}
