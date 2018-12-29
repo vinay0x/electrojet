4952392ff8346b11a601ed838399b06338df751c
Vinay <vinay@surveysparrow.com>
2018-12-29 18:22:23 +0530

    Auto resolving JSX so, let's remove extension

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
 
 import store from './store'
-import App from './App.jsx'
+import App from './App'
 
 ReactDOM.render(
   <Provider store={store}>
