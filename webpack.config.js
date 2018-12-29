50987b53ab0963a66eefc61c079e6f12b74e11dc
Vinay <vinay@surveysparrow.com>
2018-12-29 12:04:41 +0530

    Added HTML Loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,29 @@
+const HtmlWebPackPlugin = require("html-webpack-plugin");
 module.exports = {
   module: {
     rules: [
       {
-        test: /\.(js|jsx)$/,
+        test: /\.js$/,
         exclude: /node_modules/,
         use: {
           loader: "babel-loader"
         }
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: "html-loader",
+            options: { minimize: true }
+          }
+        ]
       }
     ]
-  }
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: "./app/renderer/index.html",
+      filename: "./index.html"
+    })
+  ]
 };
