681f1b2134a8089701027395797aab3cad8431b9
Vinay <vinay@surveysparrow.com>
2018-12-29 17:48:03 +0530

    Restructed store.js to export redux store by default

--- src/index.js
+++ src/index.js
@@ -2,10 +2,10 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
 
-import createStore from './store'
+import store from './store'
 import App from './App.jsx'
 
 ReactDOM.render(
-  <Provider store={createStore()}>
+  <Provider store={store}>
     <App />
   </Provider>, document.getElementById('app'))
--- src/store.js
+++ src/store.js
@@ -2,12 +2,10 @@ import { createStore, applyMiddleware, compose } from 'redux'
 import thunk from 'redux-thunk'
 
 import rootReducer from './reducers'
-export default function configureStore () {
-  return createStore(
-    rootReducer,
-    compose(
-      applyMiddleware(thunk),
-      window.__REDUX_DEVTOOLS_EXTENSION__ ? window.__REDUX_DEVTOOLS_EXTENSION__() : () => null
-    )
+export default createStore(
+  rootReducer,
+  compose(
+    applyMiddleware(thunk),
+    window.__REDUX_DEVTOOLS_EXTENSION__ ? window.__REDUX_DEVTOOLS_EXTENSION__() : () => null
   )
-}
+)
